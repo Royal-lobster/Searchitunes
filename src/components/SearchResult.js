24b924798e081510577472b335255cd0ca49ad10
Royal-lobster <srujangs8@gmail.com>
2020-09-16 11:45:08 +0530

    added small detaile

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -10,6 +10,13 @@ const Card = styled.div`
   border-radius: 10px;
   max-width: 500px;
   padding: 8px;
+  transition: all 0.6s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #222;
+    color: #eee;
+  }
 `;
 
 const Details = styled.div`
@@ -17,20 +24,26 @@ const Details = styled.div`
   font-family: "Lato", sans-serif;
 `;
 
-function SearchResult({ trackArt, trackName, artistName }) {
+function SearchResult({ trackArt, trackName, artistName, trackLink }) {
   return (
     <div className="searchResult">
-      <Card>
-        <img
-          className="searchResult__img"
-          src={trackArt}
-          alt={`${trackName}`}
-        />
-        <Details>
-          <h3 className="searchResult__title">{trackName}</h3>
-          <p className="searchResult__artistName">{artistName}</p>
-        </Details>
-      </Card>
+      <a href={trackLink} target="_blank">
+        <Card>
+          <img
+            className="searchResult__img"
+            src={trackArt}
+            alt={`${trackName}`}
+          />
+          <Details>
+            <p className="searchResult__title">
+              {trackName.replace(/^(.{20}[^\s]*).*/, "$1")}
+            </p>
+            <p className="searchResult__artistName">
+              {artistName.replace(/^(.{20}[^\s]*).*/, "$1")}
+            </p>
+          </Details>
+        </Card>
+      </a>
     </div>
   );
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,12 +6,18 @@ import illustration from "../assets/search-illustration.png";
 function SearchResults({ results }) {
   return (
     <div className="searchResults">
+      {results ? (
+        <div className="searchResults__inst">Click a track to Preview</div>
+      ) : (
+        <p></p>
+      )}
       {results?.map((result) => (
         <SearchResult
           key={result.trackId + result.artistId}
           trackArt={result.artworkUrl100}
           trackName={result.trackName}
           artistName={result.artistName}
+          trackLink={result.previewUrl}
         />
       ))}
       {!results ? (
