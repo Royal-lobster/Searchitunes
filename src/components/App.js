d33534870dfe5562ee3f01726ca3e07f65a43cd5
Royal-lobster <srujangs8@gmail.com>
2020-09-16 12:45:15 +0530

    added dark mode function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import { Layout } from "antd";
 import React from "react";
+import "./App.css";
 import Footer from "./Footer";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
-
+import themeSwitch from "../assets/half.svg";
 function App() {
   return (
     <Layout>
+      <img
+        src={themeSwitch}
+        className="app__themeSwitch"
+        alt="switch theme"
+        onClick={() => {
+          if (document.querySelector("html").getAttribute("theme") == null) {
+            document.querySelector("html").setAttribute("theme", "dark-mode");
+          } else {
+            document.querySelector("html").removeAttribute("theme");
+          }
+        }}
+      />
       <Search />
       <SearchResults />
       <Footer />
